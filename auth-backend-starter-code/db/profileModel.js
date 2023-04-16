22c56b377cf696ca2fb3dcb25138718411ac642c
Amit Kumar Mohanty <105871364+Amitkumarmohanty11@users.noreply.github.com>
2023-04-16 23:38:06 +0530

    Backend

---
+++ auth-backend-starter-code/db/profileModel.js
@@ -0,0 +1,29 @@
+// const mongoose = require('mongoose');
+
+// //profile schema
+
+// const ProfileSchema = new mongoose.Schema({
+//     //name field
+//     name:{
+//         type:String,
+//         required: [true, "Please provide a name"],
+//         unique: false,
+//     },
+    
+//     //email field 
+//     email:{
+//         type:String,
+//         required: [true, "Please provide an email"],
+//         unique: [true, 'Email Exist']
+//     },
+
+    
+
+
+
+    
+// })
+
+// //export UserSchema
+// //create a usertable if there is no table with that name already
+// module.exports = mongoose.model.Profiles || mongoose.model("Profiles",ProfileSchema)
---
+++ auth-backend-starter-code/db/userModel.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+//user schema
+
+const UserSchema = new mongoose.Schema({
+    //name field
+    name:{
+        type:String,
+        required: [true, "Please provide a name"],
+        unique: false,
+    },
+    //phoneno field
+    phone:{
+        type:Number,
+        required: [true, "Please provide a phone no"],
+        unique: [true, 'Number Exist']
+
+    },
+    //email field 
+    email:{
+        type:String,
+        required: [true, "Please provide an email"],
+        unique: [true, 'Email Exist']
+    },
+
+    //password fiald 
+    password: {
+        type:String,
+        required: [true, 'Please provide a password'],
+        unique: false,
+    },
+
+
+
+    
+})
+
+//export UserSchema
+//create a usertable if there is no table with that name already
+module.exports = mongoose.model.Users || mongoose.model("Users",UserSchema)
